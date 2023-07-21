95bf699dd37c8af6bbe9f5d051ec5edda42f095a
AyaAbdelmoghith <105630381+AyaAbdelmoghith@users.noreply.github.com>
2023-07-21 20:23:22 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ else {
 }
 submitBtn.addEventListener("click", addBookmark);
 function addBookmark() {
-    console.log(searchRepeatedName(),"print");
     if (nameValidation() && urlValidation() && searchRepeatedName()) {
         clearIcon();
         let bookmark = {
@@ -22,9 +21,10 @@ function addBookmark() {
         localStorage.setItem('bookmarksList', JSON.stringify(bookmarksList));
         reset();
         display();
-        console.log("*", bookmarksList);
     }
     else {
+        showIconNameInput();
+        showIconURLInput();
         sweetAlert();
     }
 }
@@ -49,11 +49,9 @@ function display() {
     tableBody.innerHTML = showResult;
 }
 function visitBookmark(index) {
-    console.log("hhhh", index)
     window.open(bookmarksList[index].sURL, '_blank');
 }
 function deleteBookmark(index) {
-    console.log("wwww", index)
     bookmarksList.splice(index, 1);
     localStorage.setItem('bookmarksList', JSON.stringify(bookmarksList));
     display();
@@ -63,86 +61,76 @@ siteName.addEventListener('blur', nameValidation);
 
 function nameValidation() {
     let regexName = /^[A-Za-z]{3}/;//To start with capital or small 3 letters at least.
-    var check1= regexName.test(siteName.value);
-    console.log("name",check1);
     return regexName.test(siteName.value);
 }
-siteURL.addEventListener('input',urlValidation);
-siteURL.addEventListener('blur',urlValidation);
+siteURL.addEventListener('input', urlValidation);
+siteURL.addEventListener('blur', urlValidation);
 
 function urlValidation() {
     let regexURL = /^https:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
-    var check2= regexURL.test(siteURL.value);
-    console.log("url",check2);
+    /* is designed to match and validate URLs that start with https:// and have a domain name consisting of letters (both upper and lower case), numbers, hyphens, and periods. It also allows for optional "www." at the beginning of the domain.*/
     return regexURL.test(siteURL.value);
 }
 siteName.addEventListener('input', searchRepeatedName);
 siteName.addEventListener('blur', searchRepeatedName);
 
-function searchRepeatedName(){
+function searchRepeatedName() {
     for (let i = 0; i < bookmarksList.length; i++)
-        if(bookmarksList[i].sName.includes(siteName.value))
+        if (bookmarksList[i].sName.includes(siteName.value))
             return false;//mans invalid
-   return true;//means valid
+    return true;//means valid
 }
 siteName.addEventListener('input', showIconNameInput);
 siteName.addEventListener('blur', showIconNameInput);
-siteURL.addEventListener('input',showIconURLInput);
-siteURL.addEventListener('blur',showIconURLInput);
-function showIconNameInput(){
-    if(nameValidation() && searchRepeatedName()){
-
-        console.log("once or more1")
+siteURL.addEventListener('input', showIconURLInput);
+siteURL.addEventListener('blur', showIconURLInput);
+function showIconNameInput() {
+    if (nameValidation() && searchRepeatedName()) {
         siteName.classList.add('valid');
-        siteName.classList.remove('invalid'); 
-        siteName.style.borderColor='#198754';
-        siteName.style.boxShadow='0 0 0 0.25rem rgba(25,135,84,.25)';
+        siteName.classList.remove('invalid');
+        siteName.style.borderColor = '#198754';
+        siteName.style.boxShadow = '0 0 0 0.25rem rgba(25,135,84,.25)';
     }
-    else{
-        console.log("once or more")
+    else {
         siteName.classList.add('invalid');
         siteName.classList.remove('valid');
-        siteName.style.borderColor='#dc3545';
-        siteName.style.boxShadow='0 0 0 0.25rem rgba(220,53,69,.25)';
+        siteName.style.borderColor = '#dc3545';
+        siteName.style.boxShadow = '0 0 0 0.25rem rgba(220,53,69,.25)';
     }
 }
-function showIconURLInput(){
-    if(urlValidation()){
-        console.log("here are")
+function showIconURLInput() {
+    if (urlValidation()) {
         siteURL.classList.add('valid');
-        siteURL.classList.remove('invalid'); 
-        siteURL.style.borderColor='#198754';
-        siteURL.style.boxShadow='0 0 0 0.25rem rgba(25,135,84,.25)';
+        siteURL.classList.remove('invalid');
+        siteURL.style.borderColor = '#198754';
+        siteURL.style.boxShadow = '0 0 0 0.25rem rgba(25,135,84,.25)';
     }
-    else{
-        console.log("here no")
+    else {
         siteURL.classList.add('invalid');
         siteURL.classList.remove('valid');
-        siteURL.style.borderColor='#dc3545';
-        siteURL.style.boxShadow='0 0 0 0.25rem rgba(220,53,69,.25)';
+        siteURL.style.borderColor = '#dc3545';
+        siteURL.style.boxShadow = '0 0 0 0.25rem rgba(220,53,69,.25)';
     }
 }
-function clearIcon(){
+function clearIcon() {
     siteName.classList.remove('valid');
-    siteName.style.borderColor='#d99c39';
-    siteName.style.boxShadow='0 0 0 0.25rem #fec26055';
+    siteName.style.borderColor = '#d99c39';
+    siteName.style.boxShadow = '0 0 0 0.25rem #fec26055';
     siteURL.classList.remove('valid');
-    siteURL.style.borderColor='#d99c39';
-    siteURL.style.boxShadow='0 0 0 0.25rem #fec26055';
+    siteURL.style.borderColor = '#d99c39';
+    siteURL.style.boxShadow = '0 0 0 0.25rem #fec26055';
 }
 
-function sweetAlert(){
+function sweetAlert() {
     Swal.fire({
         icon: 'error',
         title: 'Site Name or Url is not valid',
         html: `
-   
-        <p>Please follow the rules below :</p>
+           <p>Please follow the rules below :</p>
         <div class="sweetAlert-rules">
         <p class="text-left"><span class="fa fa-arrow-right"></span> Site name must contain at least 3 characters</p>
         <p class="text-left"><span class="fa fa-arrow-right"></span> Site URL must be a valid one</p>
          </div>
         `,
-        // footer: '<a href="">Why do I have this issue?</a>'
-      });
+    });
 }
